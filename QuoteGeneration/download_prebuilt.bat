ca085c3a3aa29c6fc51b061214cc360dc782cd8d
lzha101 <lili.z.zhang@intel.com>
2019-01-15 14:24:10 +0800

    Update download_prebuilt.bat
    Change prebuilt package name
    
    Signed-off-by: Zhang Lili Z lili.z.zhang@intel.com

--- QuoteGeneration/download_prebuilt.bat
+++ QuoteGeneration/download_prebuilt.bat
@@ -29,8 +29,8 @@
 
 @echo off
 
-set ae_file_name=prebuilt_dcap_1.0.1_Windows.zip
-set checksum_file=SHA256SUM_prebuilt_dcap_1.0.1_Windows.txt
+set ae_file_name=prebuilt_dcap_1.0.1_windows.zip
+set checksum_file=SHA256SUM_prebuilt_dcap_1.0.1_windows.txt
 set server_url_path=https://download.01.org/intel-sgx/dcap-1.0.1-Windows/
 set server_ae_url=%server_url_path%/%ae_file_name%
 set server_checksum_url=%server_url_path%/%checksum_file%
