955a480fd2ee19d84775112eda38d8e932e56029
Li, Xun <xun.li@intel.com>
2021-04-06 12:05:02 +0800

    Revert "Intel(R) SGX DCAP 1.10.1 Release"
    DCAP 1.10.1 is broken.
    
    This reverts commit 3ab252ab2de5d96866ea101275b75233af1ad801.
    
    Signed-off-by: Li, Xun <xun.li@intel.com>

--- QuoteGeneration/common/inc/internal/se_version.h
+++ QuoteGeneration/common/inc/internal/se_version.h
@@ -28,10 +28,10 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  */
-#define STRFILEVER    "1.10.101.2"
+#define STRFILEVER    "1.10.100.4"
 #define COPYRIGHT      "Copyright (C) 2021 Intel Corporation"
-#define FILEVER        1,10,101,2
-#define PRODUCTVER     1,10,101,2
-#define STRPRODUCTVER  "1.10.101.2"
+#define FILEVER        1,10,100,4
+#define PRODUCTVER     1,10,100,4
+#define STRPRODUCTVER  "1.10.100.4"
 #define COMPANYNAME    "Intel Corporation"
 #define PRODUCTNAME    "Intel® Software Guard Extensions"
