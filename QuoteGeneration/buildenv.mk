c68d211e3e805d4518be156a676468d71e34a07d
Xiangquan, Liu <xiangquan.liu@intel.com>
2024-01-08 13:43:12 +0800

    Implement the proposed solution of library versioning (#362)
    * Implement the proposed solution of library versioning

--- QuoteGeneration/buildenv.mk
+++ QuoteGeneration/buildenv.mk
@@ -48,11 +48,25 @@ my-dir = $(call parent-dir,$(lastword $(MAKEFILE_LIST)))
 ROOT_DIR              := $(call my-dir)
 COMMON_DIR            := $(ROOT_DIR)/common
 
-SGX_VER:= $(shell awk '$$2 ~ /STRFILEVER/ { print substr($$3, 2, length($$3) - 2); }' $(COMMON_DIR)/inc/internal/se_version.h)
-SGX_MAJOR_VER:= $(shell echo $(SGX_VER) |awk -F. '{print $$1}')
-SPLIT_VERSION=$(word $2,$(subst ., ,$1))
+#--------------------------------------------------------------------------------------
+# Function: get_full_version
+# Arguments: 1: the version name of library
+# Returns: Return the full version.
+#---------------------------------------------------------------------------------------
+get_full_version = $(shell awk '$$2 ~ /$1/ { print substr($$3, 2, length($$3) - 2); }' $(COMMON_DIR)/inc/internal/se_version.h)
+
+#--------------------------------------------------------------------------------------
+# Function: get_major_version
+# Arguments: 1: the version name of library
+# Returns: Return the major version.
+#---------------------------------------------------------------------------------------
+get_major_version = $(word 1,$(subst ., ,$(call get_full_version,$1)))
+
+SGX_VER:= $(call get_full_version,STRFILEVER)
+SGX_MAJOR_VER:= $(call get_major_version,STRFILEVER)
 
 CP    := cp -f
+LN    := ln -sf
 MKDIR := mkdir -p
 STRIP := strip
 OBJCOPY := objcopy
